e700aff988d33c09a0f352b9d94e2e46792d23eb
DicksonLewis <dickylewiss@gmail.com>
2024-04-02 11:25:12 +0300

    comments out the optional list as i have my own list

--- src/index.js
+++ src/index.js
@@ -15,3 +15,27 @@
     });
   });
   
+  // //movie menu display and user interactivity
+  // document.addEventListener("DOMContentLoaded", function() {
+  //   const filmsList = document.getElementById('films');
+  
+  //   // Make a GET request to retrieve film data
+  //   fetch('http://localhost:3000/films')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       // Remove the placeholder li element
+  //       filmsList.innerHTML = '';
+  
+  //       // Populate the films list
+  //       data.forEach(film => {
+  //         const filmItem = document.createElement('li');
+  //         filmItem.classList.add('film-item');
+  //         filmItem.textContent = film.title;
+  //         filmsList.appendChild(filmItem);
+  //       });
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });
+  // });
+  
